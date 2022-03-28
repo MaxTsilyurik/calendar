68099ab7c8844abd6f60618259f19ad8ca0e9a0f
Max Tsilyurik <tsilyurik.maxim@gmail.com>
2022-03-29 00:05:03 +0300

    add testing get by day

--- internal/adapter/repository/mock/mock_event_repository_test.go
+++ internal/adapter/repository/mock/mock_event_repository_test.go
@@ -140,4 +140,84 @@ func TestEventInMockRepository(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("getting event by day", func(t *testing.T) {
+		t.Parallel()
+		date := time.Date(2022, time.March, 22, 15, 00, 46, 0, time.UTC)
+		dateEnd := date.Add(5 * time.Minute)
+
+		eventOne, _ := calendar.NewEvent(uuid.NewString(), uuid.NewString(), "new string", "new string",
+			date, dateEnd, calendar.TimeOfEvent)
+
+		eventTwo, _ := calendar.NewEvent(uuid.NewString(), uuid.NewString(), "new string update", "new string update",
+			date, dateEnd, calendar.TimeOfEvent)
+
+		eventThree, _ := calendar.NewEvent(uuid.NewString(), uuid.NewString(), "new string update", "new string update",
+			date.AddDate(0, 1, 2), dateEnd.AddDate(0, 1, 2), calendar.TimeOfEvent)
+
+		eventFour, _ := calendar.NewEvent(uuid.NewString(), uuid.NewString(), "new string update", "new string update",
+			date.AddDate(0, 0, 2), dateEnd.AddDate(0, 0, 2), calendar.TimeOfEvent)
+
+		listSave := []*calendar.Event{eventOne, eventTwo, eventThree, eventFour}
+
+		findEvent := map[string]*calendar.Event{
+			eventOne.Id(): eventOne,
+			eventTwo.Id(): eventTwo,
+		}
+
+		testCases := []struct {
+			Name           string
+			DateSearch     time.Time
+			CountFindEvent int
+			FindEvent      map[string]*calendar.Event
+			Err            error
+		}{
+			{
+				Name:           "not found",
+				DateSearch:     date.AddDate(0, 0, 10),
+				CountFindEvent: 0,
+				FindEvent:      nil,
+				Err:            app.ErrEventNotFound,
+			},
+			{
+				Name:           "should find events by date",
+				DateSearch:     date,
+				CountFindEvent: 2,
+				FindEvent:      findEvent,
+				Err:            nil,
+			},
+		}
+
+		for _, event := range listSave {
+			_ = repository.Save(context.TODO(), event)
+		}
+
+		for i := range testCases {
+			testCase := testCases[i]
+			t.Run(testCase.Name, func(t *testing.T) {
+				t.Parallel()
+				events, err := repository.GetEventByDay(context.TODO(), testCase.DateSearch)
+
+				if err != nil {
+					require.Error(t, err)
+					require.True(t, errors.Is(err, testCase.Err))
+					return
+				}
+				require.Equal(t, testCase.CountFindEvent, len(events))
+
+				for _, event := range events {
+					if value, ok := testCase.FindEvent[event.Id()]; ok {
+						require.Equal(t, value.Id(), event.Id())
+						require.Equal(t, value.CreatedUser(), event.CreatedUser())
+						require.Equal(t, value.EventDuration(), event.EventDuration())
+						require.Equal(t, value.TimeAndDateEvent(), event.TimeAndDateEvent())
+						require.Equal(t, value.Title(), event.Title())
+						require.Equal(t, value.Description(), event.Description())
+						require.Equal(t, value.Reminder(), event.Reminder())
+					}
+				}
+
+			})
+		}
+	})
 }
